86a2a01cfbe6d302d922b3f9aa898ae6cd79b2b2
Jelena Golemac <golemac.jelena@nsoft.com>
2019-06-06 15:09:51 +0200

    reset uploader input after file remove #25

--- src/components/VUploader.vue
+++ src/components/VUploader.vue
@@ -99,6 +99,7 @@ export default {
       const idxToRemove = this.files.findIndex(file => file.name === item.name);
       this.files.splice(idxToRemove, 1);
       this.$emit('itemRemoved', item);
+      this.$refs.input.value = '';
     },
     onItemUpload(item) {
       this.$emit('itemUploaded', item);
